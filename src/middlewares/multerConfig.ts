e93524bc6770d34a2779d7a3a8056510fc2d43ce
Eliria Dias <eliriadias@gmail.com>
2024-11-21 18:28:02 -0300

    corrigindo file path

--- src/middlewares/multerConfig.ts
+++ src/middlewares/multerConfig.ts
@@ -1,14 +1,14 @@
 import multer from 'multer';
 import path from 'path';
+import { v4 as uuid } from 'uuid';
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, path.resolve(__dirname, '../../public/uploads')); 
     },
     filename: (req, file, cb) => {
-        const originalName = file.originalname;
-        
-        cb(null, originalName);
+        const fileName = `${uuid()}_${file.originalname}`;
+        cb(null, fileName);
     }
 });
 
